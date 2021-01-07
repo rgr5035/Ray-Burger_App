532b450c25bb2ec830b982c2c96353c8bcf2c3a8
Robby Ray <rgr5035@gmail.com>
2021-01-07 16:56:32 -0700

    added initial files and directory set-up

---
+++ burger/db/schema.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    burger_name VARCHAR(100) NOT NULL,
+    devoured BOOLEAN DEFAULT false
+)
+
+SELECT * FROM burgers;
---
+++ burger/db/seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
+INSERT INTO burgers (burger_name) VALUES ('Baconator');
+INSERT INTO burgers (burger_name) VALUES ('Turkey Burger'); 
