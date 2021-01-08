2b6f520cc14d178877bf2901a68f31b259fe9cd4
Robby Ray <rgr5035@gmail.com>
2021-01-07 17:14:15 -0700

    added more directory and connection set-up files

--- burger/config/connection.js
+++ burger/config/connection.js
@@ -1,1 +1,23 @@
+//declaring mySQL variable to require npm package for app functionality
 const mysql = require('mysql');
+
+//setting up connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'N!cholas15', //use mySQL specific password
+    database: 'burgers_db',
+});
+
+//make connection
+connection.connect((err) => {
+    if (err) {
+        console.log(`error connecting: ${err.stack}`);
+        return;
+    }
+    console.log(`connected as id ${connection.threadId}`);
+})
+
+//export connection for our ORM to use
+module.exports = connection;
---
+++ burger/config/orm.js
@@ -0,0 +1,16 @@
+const connection = require("./connection.js");
+
+const orm = {
+    selectAll() {
+
+    },
+    insertOne() {
+
+    },
+    updateOne() {
+
+    }
+};
+
+module.exports = orm;
+
