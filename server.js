3838484c101b4b5b0e66a3588c05b0abb6b01ca9
Robby Ray <rgr5035@gmail.com>
2021-01-08 16:24:07 -0700

    added more code to server.js and started to set up initial styling for index.handlebars

--- server.js
+++ server.js
@@ -6,3 +6,22 @@ const app = express();
 
 app.use(express.static('public'));
 
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set Handlebars.
+const exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// Import routes and give the server access to them.
+const routes = require('./burger/controllers/burgers_controllers');
+
+app.use(routes);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, () =>
+  console.log(`Server listening on: http://localhost:${PORT}`)
+);
